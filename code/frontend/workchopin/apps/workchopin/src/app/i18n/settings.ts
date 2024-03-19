4985c3b5dda7b1660fa3d20a542231dac647c380
Miłosz Kowalski <miloszkowalski@protonmail.com>
2024-03-19 22:05:55 +0100

    Add i18n support for server components

--- code/frontend/workchopin/apps/workchopin/src/app/i18n/settings.ts
+++ code/frontend/workchopin/apps/workchopin/src/app/i18n/settings.ts
@@ -1,3 +1,15 @@
 export const fallbackLng = 'en';
 export const languages = [fallbackLng, 'pl'];
+export const defaultNS = 'common';
 export const cookieName = 'i18next';
+
+export function getOptions(lng = fallbackLng, ns = defaultNS) {
+  return {
+    supportedLngs: languages,
+    fallbackLng,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns,
+  };
+}
