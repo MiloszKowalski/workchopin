3f7ed9c60f9cc24d6b36530e585724c750b0f727
Miłosz Kowalski <miloszkowalski@protonmail.com>
2024-03-24 16:37:40 +0100

    Fix invalid i18n library types export

--- code/frontend/workchopin/libs/shared/workchopin-i18n/src/index.ts
+++ code/frontend/workchopin/libs/shared/workchopin-i18n/src/index.ts
@@ -1,2 +1,3 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
+export * from './lib/@types/i18n';
 export * from './lib/i18n';
