c74de7ec4c59cb339ce1a1f3ab741803cb2dd3b2
Miłosz Kowalski <miloszkowalski@protonmail.com>
2024-03-29 00:03:12 +0100

    Add client side language select

--- code/frontend/workchopin/libs/shared/workchopin-core/src/lib/components/Menus/LanguageSelect/LanguageSelect.tsx
+++ code/frontend/workchopin/libs/shared/workchopin-core/src/lib/components/Menus/LanguageSelect/LanguageSelect.tsx
@@ -1,28 +1,61 @@
 'use client';
 
 import * as Select from '@radix-ui/react-select';
-import { CheckIcon, ChevronDownIcon } from '@workchopin/workchopin-core';
+import { languageNames } from '@workchopin/workchopin-i18n';
+import { useTranslation } from '@workchopin/workchopin-i18n/client';
+import { usePathname, useRouter } from 'next/navigation';
+import { CheckIcon, ChevronDownIcon } from '../../../icons';
+import { useMemo, useState } from 'react';
 
-export const LanguageSelect = () => (
-  <Select.Root defaultValue="pl">
-    <Select.Trigger className="inline-flex h-9 items-center justify-center gap-1 self-end rounded-lg border-2 border-black bg-white px-4 text-sm leading-[inherit] text-black shadow-sm hover:bg-gray-100 radix-placeholder:text-black">
-      <Select.Value />
-      <Select.Icon className="overflow-visible [&_svg]:w-4">
-        <ChevronDownIcon />
-      </Select.Icon>
-    </Select.Trigger>
+export const LanguageSelect = () => {
+  const { i18n } = useTranslation();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [activeLanugage, setActiveLanguage] = useState(i18n.language);
+  const otherLanguages = useMemo(
+    () =>
+      Array.from(languageNames.entries())
+        .filter(([code]) => code !== activeLanugage)
+        .sort(([_aKey, aName], [_bKey, bName]) => aName.localeCompare(bName)),
+    [activeLanugage],
+  );
 
-    <Select.Portal>
-      <Select.Content className="overflow-hidden rounded-md border-2 border-black bg-white">
-        <Select.Viewport className="p-1">
-          <SelectItem value="en">English</SelectItem>
-          <SelectItem value="pl">Polski</SelectItem>
-        </Select.Viewport>
-        <Select.Arrow />
-      </Select.Content>
-    </Select.Portal>
-  </Select.Root>
-);
+  return (
+    <Select.Root
+      defaultValue={i18n.language}
+      onValueChange={async (newLanguage) => {
+        const previousLanguage = pathname.split('/')[1];
+        router.push(pathname.replace(`/${previousLanguage}`, `/${newLanguage}`));
+        await i18n.changeLanguage(newLanguage);
+        setActiveLanguage(newLanguage);
+      }}
+    >
+      <Select.Trigger className="inline-flex h-9 items-center justify-center gap-1 self-end rounded-lg border-2 border-black bg-white px-4 text-sm leading-[inherit] text-black shadow-sm hover:bg-gray-100 radix-placeholder:text-black">
+        <Select.Value />
+        <Select.Icon className="overflow-visible [&_svg]:w-4">
+          <ChevronDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+
+      <Select.Portal>
+        <Select.Content className="overflow-hidden rounded-md border-2 border-black bg-white">
+          <Select.Viewport className="p-1">
+            <SelectItem key={activeLanugage} value={activeLanugage}>
+              {languageNames.get(activeLanugage)}
+            </SelectItem>
+            <Select.Separator className="my-1 h-[2px] bg-black" />
+            {otherLanguages.map(([code, caption]) => (
+              <SelectItem key={code} value={code}>
+                {caption}
+              </SelectItem>
+            ))}
+          </Select.Viewport>
+          <Select.Arrow />
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+};
 
 const SelectItem = (props: Select.SelectItemProps) => (
   <Select.Item
--- code/frontend/workchopin/libs/shared/workchopin-i18n/src/lib/i18n/settings.ts
+++ code/frontend/workchopin/libs/shared/workchopin-i18n/src/lib/i18n/settings.ts
@@ -2,6 +2,10 @@ export const fallbackLng = 'en';
 export const languages = [fallbackLng, 'pl'];
 export const defaultNS = 'common';
 export const cookieName = 'i18next';
+export const languageNames = new Map<string, string>([
+  ['en', 'English'],
+  ['pl', 'Polski'],
+]);
 
 export function getOptions(lng = fallbackLng, ns = defaultNS) {
   return {
